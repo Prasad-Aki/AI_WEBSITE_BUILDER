d651a7efa6e620ddfa908f021c6cd37b7e38917a
Prasad Akiwate <prasadakiwate2006@gmail.com>
2026-06-20 13:12:03 +0530

    Update auth.controller.js

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -15,7 +15,7 @@ export const googleAuth = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            SameSite: "none",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -31,7 +31,7 @@ export const logOut = async (req, res) => {
         res.clearCookie("token", {
             httpOnly: true,
             secure: true,
-            SameSite: "none"
+            sameSite: "none"
         })
         return res.status(200).json({ message: "logout success" })
     } catch (error) {
