47c455f17a396d1d2bb282842eb99266eba8e998
Prasad Akiwate <prasadakiwate2006@gmail.com>
2026-07-03 18:10:40 +0530

    errors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())   // when data comes from frontend in body it is null so we use this middleware then data comes in json format
 app.use(cookieParser())
-console.log("FRONTEND_URL:", process.env.FRONTEND_URL)
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
